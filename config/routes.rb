e5ab071ae8c8a82b68a4d8873e177447da29be2f
jc <emclab2011@gmail.com>
2015-01-04 16:31:37 -0600

    JC: added search route. spec passes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 UserManualx::Engine.routes.draw do
 
-  resources :manuals
+  resources :manuals do
+    collection do
+      get :search
+      get :search_results
+    end
+  end
   root :to => 'manuals#index'
 end
